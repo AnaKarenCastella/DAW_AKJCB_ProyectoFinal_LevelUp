bf9f0a9f16dcc0250e1a12b5c3f0b54eabb4e6f1
Ana Karen Jacqueline Castella Bencomet <zS22024114@estudiantes.uv.mx>
2026-06-06 11:57:42 -0600

    Actualizacion en la clase Videojuego

--- src/main/java/com/app/model/Videojuego.java
+++ src/main/java/com/app/model/Videojuego.java
@@ -3,14 +3,18 @@ package com.app.model;
 public class Videojuego {
 
     private int idJuego;
+    private int rawgId;
     private String titulo;
     private String descripcion;
     private String plataforma;
     private String categoria;
     private double precio;
     private int stock;
-
     private String imagenUrl;
+
+    private double rating;
+    private String fechaLanzamiento;
+    private String generos;
 
     public Videojuego() {}
 
@@ -20,6 +24,14 @@ public class Videojuego {
 
     public void setIdJuego(int idJuego) {
         this.idJuego = idJuego;
+    }
+
+    public int getRawgId() {
+        return rawgId;
+    }
+
+    public void setRawgId(int rawgId) {
+        this.rawgId = rawgId;
     }
 
     public String getTitulo() {
@@ -76,5 +88,29 @@ public class Videojuego {
 
     public void setImagenUrl(String imagenUrl) {
         this.imagenUrl = imagenUrl;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getFechaLanzamiento() {
+        return fechaLanzamiento;
+    }
+
+    public void setFechaLanzamiento(String fechaLanzamiento) {
+        this.fechaLanzamiento = fechaLanzamiento;
+    }
+
+    public String getGeneros() {
+        return generos;
+    }
+
+    public void setGeneros(String generos) {
+        this.generos = generos;
     }
 }
